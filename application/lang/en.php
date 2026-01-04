e019823c2d1a5a5c6c9bf7153aa24f892961ef07
aliahmadrspsoft <aliahmad.mirzaee.rspsoft@gmail.com>
2026-01-04 20:48:52 +0330

    14041014

--- application/lang/en.php
+++ application/lang/en.php
@@ -1,5 +1,15 @@
 <?php
 return array(
     'welcome'      => 'Welcome to our site',
+    'sysname'       =>'سامانه مدیریت بحران',
+    'username'      =>'نام کاربری',
+    'password'      =>'رمز عبور',
+    "enter_user_name"=>"نام کاربری خود را وارد کنید.",
+    'notice_pass'=>' *نام کاربری و پسورد خود را در اختیار دیگران قرار ندهید*',
+    'enter_pass'=>"رمز عبور خود را با رعایت نکات ایمنی وارد کنید.",
+    'enter_sys'=>'ورود به سامانه',
+    'dashboard_sys'=>'داشبورد سامانه مدیریت بحران',
+    'dashboard'=>'داشبورد',
+    'call_crisis'=>'اعلام بحران'
    
 );
